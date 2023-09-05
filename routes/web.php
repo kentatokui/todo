0d32162031c70b1b26bcf6e6326fd4451d707eb7
kentatokui <kenta.tokui@gmail.com>
2023-09-05 13:10:09 +0900

    一時保存

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,26 @@ use App\Http\Controllers\Auth\LoginController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::group(['middleware' => 'auth'], function() {
+    // Route::get('/index', [ soundpost::class, 'index' ])->name('home.index');
+    Route::get('/folders/{id}/tasks', [ TaskController::class, 'index' ])->name('tasks.index');
 
-// Route::get('/index', [ soundpost::class, 'index' ])->name('home.index');
-Route::get('/folders/{id}/tasks', [ TaskController::class, 'index' ])->name('tasks.index');
+    Route::get('/folders/create', [ FolderController::class, 'showCreateForm' ])->name('folders.create');
 
-Route::get('/folders/create', [ FolderController::class, 'showCreateForm' ])->name('folders.create');
+    Route::post('/folders/create', [ FolderController::class, 'create' ]);
 
-Route::post('/folders/create', [ FolderController::class, 'create' ]);
+    Route::get('/folders/{id}/tasks/create', [TaskController::class,'showCreateForm'])->name('tasks.create');
 
-Route::get('/folders/{id}/tasks/create', [TaskController::class,'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', [TaskController::class,'create']);
 
-Route::post('/folders/{id}/tasks/create', [TaskController::class,'create']);
+    Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
 
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
 
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
 
 Auth::routes();
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
